433878ad07a2ab45d0f0b23f322dc15daf77aa8a
amrou <amrou@AMROU.myguest.virtualbox.org>
2024-04-23 23:37:00 +0200

    main.c

--- main.c
+++ main.c
@@ -5,35 +5,29 @@
 #include <SDL/SDL_mixer.h>
 #include "background.h"
 
-int main() 
-{
-bg background;
-initbg(&background);
-if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) 
-{
+
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 800
+
+int main() {
+if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
 printf("SDL initialization failed: %s\n", SDL_GetError());
 return 1;
 }
-if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) == -1) 
-{
-printf("SDL_mixer initialization failed: %s\n", Mix_GetError());
-return 1;
-}
-SDL_Surface *screen = SDL_SetVideoMode(1000, 800, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-if (screen == NULL) 
-{
+SDL_Surface *screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+if (screen == NULL) {
 printf("Failed to set video mode: %s\n", SDL_GetError());
+SDL_Quit();
 return 1;
 }
-Mix_Music *bgMusic = Mix_LoadMUS("mgs.mp3");
-if (bgMusic == NULL) 
-{
-printf("Failed to load music: %s\n", Mix_GetError());
-} else 
-{
-Mix_PlayMusic(bgMusic, -1);
-}
+bg background;
+initbg(&background);
+image heart;
+imgheart(&heart);
+Mix_Music *bgMusic;
+musicLoad1(bgMusic);
 int quit = 0;
+int scrollingRight = 0, scrollingLeft = 0, scrollingUp = 0, scrollingDown = 0;
 SDL_Event event;
 while (!quit) 
 {
@@ -42,30 +36,63 @@ while (SDL_PollEvent(&event))
 if (event.type == SDL_QUIT) 
 {
 quit = 1;
-} 
+}
 else if (event.type == SDL_KEYDOWN) 
 {
 switch (event.key.keysym.sym) 
 {
 case SDLK_RIGHT:
-scrolling(&background, 0, 10, 0);
+scrollingRight = 1;
 break;
 case SDLK_LEFT:
-scrolling(&background, 1, 10, 0);
+scrollingLeft = 1;
 break;
 case SDLK_UP:
-scrolling(&background, 2, 0, 10);
-break;               
+scrollingUp = 1;
+break;
 case SDLK_DOWN:
-scrolling(&background, 3, 0, 10);
-break;
-default:
+scrollingDown = 1;
 break;
 }
 }
+else if (event.type == SDL_KEYUP) 
+{
+switch (event.key.keysym.sym) 
+{
+case SDLK_RIGHT:
+scrollingRight = 0;
+break;
+case SDLK_LEFT:
+scrollingLeft = 0;
+break;
+case SDLK_UP:
+scrollingUp = 0;
+break;
+case SDLK_DOWN:
+scrollingDown = 0;
+break;
+}
+}
+}
+if (scrollingRight) 
+{
+scrolling(&background, 0, 10, 0); 
+}
+if (scrollingLeft) 
+{
+scrolling(&background, 1, 10, 0); 
+}
+if (scrollingUp) 
+{
+scrolling(&background, 2, 0, 10); 
+}
+if (scrollingDown)
+{
+scrolling(&background, 3, 0, 10); 
 }
 SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0));
 afficherbg(&background, screen);
+afficherheart(&heart, screen);
 SDL_Flip(screen);
 SDL_Delay(10);
 }
@@ -73,3 +100,5 @@ Mix_FreeMusic(bgMusic);
 SDL_Quit();
 return 0;
 }
+
+
