e70ddff296486de20bf9f8626454c4f66c74f8e1
omarfh111 <Omar.Fkihhassen@esprit.tn>
2024-05-12 18:32:07 +0100

    ajout

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "perso.h"
+
+void quit(int * q) {
+  Uint8 * keystate = SDL_GetKeyState(NULL);
+  if (keystate[SDLK_ESCAPE])( * q) = 1;
+}
+int main() {
+  perso p1, p2;
+  init_perso( & p1);
+  init_perso_controls( & p1);
+  init_images_perso( & p1,"spritesheet.png");
+  init_perso( & p2);
+  init_perso_controls_p2( & p2);
+  init_images_perso( & p2,"spritesheet1.png");
+  SDL_Init(SDL_INIT_EVERYTHING);
+  TTF_Init();
+  SDL_Surface * screen = SDL_SetVideoMode(1000, 800, 32, SDL_SWSURFACE);
+  SDL_Surface * bg;
+  bg = IMG_Load("bg.png");
+  int q = 0;
+  while (q == 0) {
+    SDL_PumpEvents();
+    quit( & q);
+    SDL_Delay(60);
+    SDL_BlitSurface(bg, NULL, screen, NULL);
+    afficher_perso(p1, screen);
+    afficher_score_vie(p1, screen, 0);
+    animer_perso( & p1);
+    move_perso( & p1);
+    player_limite( & p1, screen);
+    saut_perso( & p1);
+    afficher_perso(p2, screen);
+    afficher_score_vie(p2, screen, 1);
+    animer_perso( & p2);
+    move_perso( & p2);
+    player_limite( & p2, screen);
+    saut_perso( & p2);
+    SDL_Flip(screen);
+  }
+}
